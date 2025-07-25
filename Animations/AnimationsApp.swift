a28934c014dfbb97f1c3c8a317368b95b387e73e
aajuvale <ahmed.juvale@gmail.com>
2025-07-25 09:38:29 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ahmed Juvale on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
