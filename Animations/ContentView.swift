d3e2bdd5b1576a10b3e11090748728b013de125a
aajuvale <ahmed.juvale@gmail.com>
2025-07-25 09:53:40 -0700

    feat: implemented scale amount

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,26 +13,19 @@ struct ContentView: View {
     private var animationAmount = 1.0
 
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                    .repeatForever(autoreverses: true),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+        VStack {
+            Stepper("Scale Amount", value: $animationAmount.animation(), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
     }
 }
